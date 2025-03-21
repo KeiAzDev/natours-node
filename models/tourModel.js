cbb0f4c1417220ec8bd625ad170d922b84b084e3
KeiAzDev <enopiyo3code+work@gmail.com>
2025-03-20 21:28:12 -0700

    Made modelc
    mongooseでのスキーマ、modelの作成

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,8 +5,25 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
   },
@@ -14,6 +31,26 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 const Tour = mongoose.model('Tour', tourSchema);
 
